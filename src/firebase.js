90db4ac3aa710c88f64849883de35b68f949c2ea
ShahbDadKhan <shahabkhan681@gmail.com>
2021-09-09 06:31:21 +0530

    working

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDjTr6mIGvuXD5WxKMaJsvnc77OdhcKhdo",
+  authDomain: "portfolio-7956c.firebaseapp.com",
+  projectId: "portfolio-7956c",
+  storageBucket: "portfolio-7956c.appspot.com",
+  messagingSenderId: "926087712247",
+  appId: "1:926087712247:web:1880a9b573a8599dfff2f2",
+  measurementId: "G-SX5BLF1GHD",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.getAnalytics();
