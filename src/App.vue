fc2354f8b3e434dbd89c5f6087eed35abae04842
shahabdadkhan <shahabkhan@fynd-external.com>
2022-12-27 19:05:29 +0530

    working on my brief info

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,11 @@
   <v-app>
     <v-main>
       <app-bar />
-      <my-intro />
+      <!-- <my-intro />
       <about-me />
-      <my-portfolio />
+      <my-portfolio /> -->
       <!-- <contact-me /> -->
-      <!-- <router-view /> -->
+       <router-view />
       <the-footer />
     </v-main>
   </v-app>
@@ -15,17 +15,17 @@
 <script>
 import AppBar from "./components/AppBar.vue";
 import TheFooter from "./components/TheFooter.vue";
-import AboutMe from "./views/AboutMe.vue";
-// import ContactMe from "./views/ContactMe.vue";
-import MyIntro from "./views/MyIntro.vue";
-import MyPortfolio from "./views/MyPortfolio.vue";
+// import AboutMe from "./views/AboutMe.vue";
+// // import ContactMe from "./views/ContactMe.vue";
+// import MyIntro from "./views/MyIntro.vue";
+// import MyPortfolio from "./views/MyPortfolio.vue";
 export default {
   components: {
     AppBar,
     TheFooter,
-    MyIntro,
-    AboutMe,
-    MyPortfolio,
+    // MyIntro,
+    // AboutMe,
+    // MyPortfolio,
     // ContactMe,
   },
   name: "App",
@@ -43,6 +43,24 @@ main {
   background-color: black;
   color: #fff;
   width: 100%;
+
+  // &:before {
+  //   content: ' ';
+  //   display: block;
+  //   position: absolute;
+  //   left: 0;
+  //   top: 0;
+  //   width: 100%;
+  //   height: 100%;
+  //   opacity: 0.2;
+  //   background-image: url('./assets/Img/Background/desktop.jpg');
+
+  //   // background-image: url('https://assets.digitalocean.com/labs/images/community_bg.png');
+  //   background-repeat: no-repeat;
+  //   background-position: 50% 0;
+  //   background-size: cover;
+  //   background-attachment: fixed;
+  // }
 
   // @media (max-width: 600px) {
   //   background-color: transparent;
